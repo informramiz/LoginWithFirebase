c30e9949b3dac0c562d47cb1c07f8e4529ed0cde
Ramiz Raja <informramiz@gmail.com>
2020-07-05 12:08:02 +0500

    chore: Create project

---
+++ LoginWithFirebase/app/src/main/java/github/informramiz/loginwithfirebase/MainActivity.kt
@@ -0,0 +1,13 @@
+package github.informramiz.loginwithfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import github.informramiz.loginwithfirebase.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
---
+++ LoginWithFirebase/app/src/main/java/github/informramiz/loginwithfirebase/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package github.informramiz.loginwithfirebase.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
