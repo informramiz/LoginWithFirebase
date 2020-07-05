536c62aee2b0ebcb246889aad82dce0822e88958
Ramiz Raja <informramiz@gmail.com>
2020-07-05 12:12:26 +0500

    feat: Enable databinding and viewBinding

--- LoginWithFirebase/app/src/main/java/github/informramiz/loginwithfirebase/ui/main/MainFragment.kt
+++ LoginWithFirebase/app/src/main/java/github/informramiz/loginwithfirebase/ui/main/MainFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import github.informramiz.loginwithfirebase.R
+import github.informramiz.loginwithfirebase.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,14 +15,15 @@ class MainFragment : Fragment() {
     }
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var viewBinding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return MainFragmentBinding.inflate(inflater, container,false).also { viewBinding = it }.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
     }
-
 }
